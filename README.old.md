54ef6215e20a0b1f831ea0b3fa104b8998de4797
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-09-30 08:26:39 +0530

    initail commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# v-NEWS
+ --- v-NEWS is news website where u find local as well as globel news 
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+    import logo from '../logo.svg';
+    import '../App.css';
+
+const Navbar = ()=>{
+    return (
+        <img src={logo} className="App-logo" alt="logo" style={{width:"100px"}}/>
+        
+
+    );
+}
+export default Navbar
