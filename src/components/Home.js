1e4989bdd29b9a5cfba4cea1fba4447bc9ecbf41
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-09-30 09:37:36 +0530

    update Home component

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+const Home = () => {
+    return (
+        <>
+            <div className="App">
+                <img src={logo} className="App-logo" alt="logo" style={{ width: "100px" }} />
+                <div className="App-header">
+                    Welcome to V-news
+                </div>
+            <button type="button" name="" id="" class="btn btn-primary" btn-lg >Next</button>
+            </div>
+
+        </>
+    );
+}
+export default Home
+
