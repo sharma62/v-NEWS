6b37dcac075293da6fc8e1c61dbee0bb8148e81a
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-11-16 08:44:12 +0530

    navbar updated

---
+++ src/components/body/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import NewsItem from "./NewsItem"
+
+export default class News extends Component {
+  render() {
+    return (
+      <>
+      
+         <NewsItem newsTitle="title" goDiscription="" />
+
+
+      </>
+    )
+  }
+}
---
+++ src/components/body/NewsItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+    render(props) {
+        return (
+            <><div className="container">
+                
+                <div class="card my-3">
+                    <h5 class="card-header">Featured</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="/" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            </>
+        )
+    }
+}
